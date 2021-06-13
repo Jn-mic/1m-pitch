4bf2e9e613feae29c66b611b1cf66d10dc83c943
jn-mic <jackotienokey@gmail.com>
2021-06-13 19:02:26 +0300

    validation for auth folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,8 +27,8 @@ def login():
     return render_template('login.html')
 
 
-@auth.route('/signup', methods=['GET', 'POST'])
-def signup():
+@auth.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
     if request.method == 'POST':
         form = request.form
         username = form.get("username")
@@ -37,29 +37,29 @@ def signup():
         confirm_password = form.get("confirm_password")
         if username is None or password is None or email is None or confirm_password is None:
             error = 'username, email, password are required'
-            return render_template('signup.html', error=error)
+            return render_template('sign_up.html', error=error)
         if ' ' in username:
             error = 'Username should not contain spaces'
-            return render_template('signup.html', error=error)
+            return render_template('sign_up.html', error=error)
         if password != confirm_password:
             error = "Passwords do not match"
-            return render_template('signup.html', error=error)
+            return render_template('sign_up.html', error=error)
         else:
             user = User.query.filter_by(username=username).first()
             if user is not None:
                 error = 'A user with that name already exists'
-                return render_template('signup.html', error=error)
+                return render_template('sign_up.html', error=error)
             user = User.query.filter_by(email=email).first()
             if user is not None:
                 error = 'A user with that email already exists'
-                return render_template('signup.html', error=error)
+                return render_template('sign_up.html', error=error)
             user = User(username=username, email=email)
             user.set_password(password)
             user.save()
             # email_message("Welcome to 1m Pitch", "email/howdy", user.email, user=user)
             return redirect(url_for('auth.login'))
 
-    return render_template('signup.html')
+    return render_template('sign_up.html')
 
 
 @auth.route('/logout')
